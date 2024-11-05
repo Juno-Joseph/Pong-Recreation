02c78c51be85d66ea07b99fbf9e2603d9cc02f5f
Juno Joseph <87766026+Juno-Joseph@users.noreply.github.com>
2024-11-04 23:23:44 -0500

    hi

---
+++ Scripts/pong_stick_player2.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+
+func _physics_process(delta):
+	#set direction movement
+	var direction = Input.get_axis("up_arrow", "down_arrow")
+	if direction:
+		velocity.y = direction * SPEED
+	else:
+		velocity.y = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
